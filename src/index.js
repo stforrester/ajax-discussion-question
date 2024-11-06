1867302ac9fc5877758f474116ffe99abda58f72
Stuart Forrester <stforrester@gmail.com>
2024-11-06 12:09:37 -0600

    complete discussion except #5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
-const fullname = document.getElementById("fullname");
+/*const fullname = document.getElementById("fullname");
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+
+console.log("Hello")
+
+fetch('https://randomuser.me/api/')
+  .then( res => res.json() )
+  .then( data => {
+    console.log("Hi") 
+  })
+
+console.log("Sup?")*/
+
+/*function fetchData(){
+  let data = fetch('https://randomuser.me/api/')
+    .then( res => res.json() )
+    .then( res => res )
+  console.log(data)
+}
+
+fetchData()*/
+
+/*function fetchData(){
+  let data = fetch('https://randomuser.me/api/')
+    .then( res => res.json() )
+    .then( json => console.log(json) )
+}
+
+fetchData()*/
+
+function fetchData(){
+  let data = fetch('https://randomuser.me/api/')
+    .then( res => res.json() )
+    .then( console.log )
+}
+
+fetchData()
